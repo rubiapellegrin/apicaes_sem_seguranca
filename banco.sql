d7ceb08c548314283afbc0b49d0adf0ab8b966ac
rubiapellegrin <rubiapellegrin96@gmail.com>
2022-10-27 19:54:07 -0300

    Seguranca

--- banco.sql
+++ banco.sql
@@ -25,3 +25,19 @@ create table caes (
 insert into caes (nome, cliente, peso, raca) 
 values ('Guri', 'Rubia', 12, 1), ('Schebaca', 'Rubia', 9, 1)
 returning codigo, nome, cliente, peso, raca;
+
+
+-- criação da tabela usuários
+create table usuarios (
+	email varchar(50) not null primary key, 
+	senha varchar(20) not null, 
+	tipo char(1)  not null, 
+	check (tipo = 'T' or tipo = 'A' or tipo = 'U'),
+	telefone varchar(14)  not null, 
+	nome varchar(50) not null
+);
+
+-- inserindo alguns registros na tabela usuários
+insert into usuarios (email, senha, tipo, telefone, nome) 
+values ('jorgebavaresco@ifsul.edu.br', '123456', 'A','(54)99984-4348','Jorge Bavaresco'), 
+('joao@ifsul.edu.br', '123456', 'U','(54)44484-4348','Joao');
