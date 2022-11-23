3a7ae113c2703130324ea40383188bc3da38e3dc
rubiapellegrin <rubiapellegrin96@gmail.com>
2022-11-23 17:11:19 -0300

    first commit

--- rotas.js
+++ rotas.js
@@ -2,31 +2,28 @@ const { Router } = require('express');
 
 const controleRacas = require('./controladores/racas');
 const controleCaes = require("./controladores/caes");
-const seguranca = require('./controladores/seguranca');
 
 const rotas = new Router();
 
 
 rotas.route('/caes')
-   .get(seguranca.verificaJWT, controleCaes.getCaes)
-   .post(seguranca.verificaJWT, controleCaes.addCao)
-   .put(seguranca.verificaJWT, controleCaes.updateCao)
+   .get(controleCaes.getCaes)
+   .post(controleCaes.addCao)
+   .put(controleCaes.updateCao)
 
 rotas.route('/caes/:codigo')
-   .get(seguranca.verificaJWT, controleCaes.getCaoPorCodigo)
-   .delete(seguranca.verificaJWT, controleCaes.deleteCao)
+   .get(controleCaes.getCaoPorCodigo)
+   .delete(controleCaes.deleteCao)
 
 
 rotas.route('/racas')
-     .get(seguranca.verificaJWT, controleRacas.getRacas)
-     .post(seguranca.verificaJWT, controleRacas.addRaca)
-     .put(seguranca.verificaJWT, controleRacas.updateRaca)
+     .get(controleRacas.getRacas)
+     .post(controleRacas.addRaca)
+     .put(controleRacas.updateRaca)
 
 rotas.route('/racas/:codigo')
-     .get(seguranca.verificaJWT, controleRacas.getRacaPorCodigo)
-     .delete(seguranca.verificaJWT, controleRacas.deleteRaca)
+     .get(controleRacas.getRacaPorCodigo)
+     .delete(controleRacas.deleteRaca)
 
-rotas.route("/login")
-     .post(seguranca.login)  
 
 module.exports = rotas;
